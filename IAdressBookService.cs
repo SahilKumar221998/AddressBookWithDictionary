6fa062947bf99c6077b2b920fd392406a20bdb96
SahilKumar221998 <sahilja265@gmail.com>
2024-06-27 11:14:32 +0530

    Interafce with all the abstract methods

---
+++ IAdressBookService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookWithDictionary
+{
+   public interface IAdressBookService
+    {
+        void dictionaryIntializers();
+        void addPerson();
+        void editPerson();
+        void removePerson();
+        void showUserDetails();
+        string userCredentialsCheck(string name);
+
+        void searchOnTheBasisOfCity();
+        void searchOnTheBasisOfState();
+    }
+}
